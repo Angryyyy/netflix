9956b150733e3d90ec5c48c42e1d18093c03484c
Angryyyy <angryyforever@gmail.com>
2023-07-11 23:53:00 +0530

    netflix file added from PC

---
+++ src/components/Cards.js
@@ -0,0 +1,23 @@
+function Cards(props){
+    return(
+
+        <>
+
+        {/* <h1 style={{padding:20}}>Trending Now </h1> */}
+
+        <div >
+
+            <img src={props.imgsrc} alt="card-image" style={{height:200,padding:20}} />
+
+
+        </div>
+        
+        
+          </>
+
+
+    )
+
+}
+
+export default Cards;
---
+++ src/components/Logo.js
@@ -0,0 +1,31 @@
+function Logo(){
+    return(
+        <>
+        
+
+        <img src="https://download.logo.wine/logo/Netflix/Netflix-Logo.wine.png" alt="netflix-logo" style={{height:80, padding:20}} />
+        
+        
+        <span style={{color:"white", padding:'10px', margin:'35px', position:"absolute", top:"13px",cursor:"pointer" }}>
+       
+
+       <span style={{color:"white", padding:'10px'}}>Home</span>
+       <span style={{color:"white", padding:'10px'}}>Tv Shows</span>
+       <span style={{color:"white", padding:'10px'}}>Movies</span>
+       <span style={{color:"white", padding:'10px'}}>New & popular</span>
+       <span style={{color:"white", padding:'10px'}}>My List</span>
+       <span style={{color:"white", padding:'10px'}}>Browse by Language</span>
+   
+       <span style={{color:"white",}}><i class="fa-solid fa-magnifying-glass" style={{color:"white",}}></i></span>
+   
+   
+       </span>
+   
+   
+   
+
+        </>
+    )
+}
+
+export default Logo;
